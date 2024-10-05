57694fbef1117d3276c40e66e42296f808de3d21
1josiah <olivoj7@gmail.com>
2024-10-05 15:58:12 -0400

    CSC 325 Lab 5 Tip Calculator

--- src/main/java/com/example/lab5tipcalculator/TipCalculator.java
+++ src/main/java/com/example/lab5tipcalculator/TipCalculator.java
@@ -12,7 +12,7 @@ public class TipCalculator extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TipCalculator.class.getResource("TipCalculator.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Josiah's Tip Calculator");
         stage.setScene(scene);
         stage.show();
     }
